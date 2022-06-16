84d87b1182458b4536b712cc790672b08e32f11b
SakaguchiTakumi03 <sakaguchi.takumi03@gmail.com>
2022-06-16 19:30:32 +0900

    写経０９、１０を作成。

---
+++ MemoPad3/app/src/main/java/local/hal/st42/android/memopad3/Consts.java
@@ -0,0 +1,19 @@
+package local.hal.st42.android.memopad3;
+
+/**
+ * ST42 Androidサンプル10 Room2
+ *
+ * 定数定義クラス。
+ *
+ * @author Shinzo SAITO
+ */
+public class Consts {
+        /**
+         * 新規登録モードを表す定数フィールド。
+         */
+        static final int MODE_INSERT = 1;
+        /**
+         * 更新モードを表す定数フィールド。
+         */
+        static final int MODE_EDIT = 2;
+}
